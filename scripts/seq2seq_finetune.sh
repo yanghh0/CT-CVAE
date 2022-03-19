32818eb739db921d9f1e5c634f1f4cf21569867d
yanghh0 <13588036019@163.com>
2022-03-19 09:32:45 +0800

    update

--- scripts/seq2seq_finetune.sh
+++ scripts/seq2seq_finetune.sh
@@ -1,5 +1,5 @@
 python ../parlai/scripts/train_model.py \
-    --task dailydialog,wizard_of_wikipedia,empathetic_dialogues,convai2,cornell_movie \
+    --task dailydialog,wizard_of_wikipedia,empathetic_dialogues,convai2 \
     --model seq2seq \
     --init-model ../../checkpoint/seq2seq/pretrain/model \
     --model-file ../../checkpoint/seq2seq/joint-fine-tuning/model \
--- scripts/transformer_finetune.sh
+++ scripts/transformer_finetune.sh
@@ -1,5 +1,5 @@
 python ../parlai/scripts/train_model.py \
-    --task dailydialog,wizard_of_wikipedia,empathetic_dialogues,convai2,cornell_movie \
+    --task dailydialog,wizard_of_wikipedia,empathetic_dialogues,convai2 \
     --model transformer/generator \
     --init-model ../../checkpoint/transformer/pretrain/model \
     --model-file ../../checkpoint/transformer/joint-fine-tuning/model \
