e8a495acf720e79be2cfd93b13b145ba4dc6726c
yanghh0 <13588036019@163.com>
2022-03-30 12:09:16 +0800

    update

---
+++ scripts/cosine_similarity.sh
@@ -0,0 +1,6 @@
+python ../parlai/scripts/eval_model.py \
+    --datatype valid \
+    --task twitter  \
+    --model bert_ranker/bi_encoder_ranker \
+    --bert-aggregation first \
+    --batchsize 1
